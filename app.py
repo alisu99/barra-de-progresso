252d4af3c7082724897dc6dfa0a91445236a1762
alisu99 <alissonsts910@gmail.com>
2024-05-09 21:58:52 -0300

    ...

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# Rota para a página HTML
+@app.route('/')
+def index():
+    return render_template('progresso.html')
+
+@app.route('/progresso')
+def obter_progresso():
+    lista = [25, 50, 18, 1, 1 ,1, 1 ,1 ,1 ,1 ,1 , 25, 25, 50, 18, 1, 1 ,1, 1 ,1 ,1 ,1 ,1 ,1 ,25, 50, 18, 1, 1 ,1, 1 ,1 ,1 ,1 ,1 ,1 , 25, 50, 18, 1, 1 ,1, 1 ,1 ,1 ,1 ,1 ,1 , 50, 18, 1, 1 ,1, 1 ,1 ,1 ,1 ,1 ,1 , 25, 50, 18, 1, 1 ,1, 1 ,1 ,1 ,1 ,1 ,1 ,]
+    numero_progresso = len(lista)
+    return jsonify({'progresso': numero_progresso})
+
+if __name__ == '__main__':
+    app.run(debug=True)
